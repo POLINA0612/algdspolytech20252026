b9575b37c0769996da68a2d8fb6adbf5a21e0296
POLINA0612 <114597592+POLINA0612@users.noreply.github.com>
2025-11-01 18:03:21 +0000

    готово

--- Homework_1_2025.c
+++ Homework_1_2025.c
@@ -2,5 +2,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-struct Human{
+struct Human {
+    int m;
+};
+
+int main(void) {
+    struct Human person;
+    person.m = 2 * 6; // Инициализация после создания структуры
+    printf("m = %d\n", person.m);
+    printf("I am old");
 }
